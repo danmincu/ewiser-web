22701bc4655deec6fe37efbd3d9fc64c26f4018e
Dan Mincu <danmincu@gmail.com>
2022-07-15 16:02:31 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 source ~/anaconda3/etc/profile.d/conda.sh
 conda activate ewiser
-python ~/ewiser-web/bin/annotate.py -c ~/ewiser-web/res/downloaded/ewiser.semcor_base.pt test.txt
+python ~/ewiser-web/bin/annotate.py -c ~/ewiser/res/downloaded/ewiser.semcor_base.pt test.txt
